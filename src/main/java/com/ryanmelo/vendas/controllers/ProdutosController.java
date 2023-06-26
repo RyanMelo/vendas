1a01ef7ad990f76bc1ff531a516bf288b88835fc
RyanMelo <ryangarciamelo@gmail.com>
2023-06-26 13:31:06 -0300

    feat: Adicionar produto

--- src/main/java/com/ryanmelo/vendas/controllers/ProdutosController.java
+++ src/main/java/com/ryanmelo/vendas/controllers/ProdutosController.java
@@ -9,12 +9,13 @@ import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ryanmelo.vendas.entity.Produto;
 import com.ryanmelo.vendas.repository.ProdutoRepository;
-
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -25,8 +26,9 @@ public class ProdutosController {
 
     @GetMapping(value = "")
     public ResponseEntity<List<Produto>> getProdutos(Produto filtro) {
-        
-        ExampleMatcher matcher = ExampleMatcher.matching().withIgnoreCase().withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+
+        ExampleMatcher matcher = ExampleMatcher.matching().withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
 
         Example<Produto> exampleProduto = Example.of(filtro, matcher);
 
@@ -35,17 +37,25 @@ public class ProdutosController {
         return ResponseEntity.ok(listProdutos);
     }
 
-    @GetMapping(value="/{id}")
+    @GetMapping(value = "/{id}")
     public ResponseEntity<Produto> getProdutoById(@PathVariable Integer id) {
 
         Optional<Produto> produto = produtoRepository.findById(id);
 
-        if(produto.isPresent()) {
+        if (produto.isPresent()) {
             return ResponseEntity.ok(produto.get());
         }
 
         return ResponseEntity.notFound().build();
     }
-    
+
+    @PostMapping(value = "")
+    public ResponseEntity<Produto> criarProduto(@RequestBody Produto produto) {
+
+        Produto produtoSalvo = produtoRepository.save(produto);
+
+        return ResponseEntity.created(null).body(produtoSalvo);
+
+    }
 
 }
