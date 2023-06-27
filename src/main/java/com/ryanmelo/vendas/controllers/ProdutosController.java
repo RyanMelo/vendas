e935a9be65307ddbcd623560cbf1364168941f9a
RyanMelo <ryangarciamelo@gmail.com>
2023-06-27 09:33:29 -0300

    feat: metodo de atualizar produto e deletar produto

--- src/main/java/com/ryanmelo/vendas/controllers/ProdutosController.java
+++ src/main/java/com/ryanmelo/vendas/controllers/ProdutosController.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,6 +58,29 @@ public class ProdutosController {
 
         return ResponseEntity.created(null).body(produtoSalvo);
 
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Produto> atualizarProduto(@PathVariable Integer id, @RequestBody Produto produto) {
+
+        return produtoRepository.findById(id).map(produtoExistente -> {
+           produto.setId(produtoExistente.getId()); 
+           produtoRepository.save(produto);
+           return ResponseEntity.ok(produto);
+        }).orElse(ResponseEntity.notFound().build());
+
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Produto> deletarProduto(@PathVariable Integer id) {
+        Optional<Produto> produto = produtoRepository.findById(id);
+
+        if (produto.isPresent()) {
+            produtoRepository.delete(produto.get());
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
 }
